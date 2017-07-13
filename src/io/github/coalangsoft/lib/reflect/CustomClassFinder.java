997af4874174e82af2b67cbb739b0b57ab865b5d
NetLamp <oplpublishergerman@gmail.com>
2017-07-13 10:08:51 +0200

    Added "resource" method to ClassFinder

--- src/io/github/coalangsoft/lib/reflect/CustomClassFinder.java
+++ src/io/github/coalangsoft/lib/reflect/CustomClassFinder.java
@@ -1,7 +1,10 @@
 package io.github.coalangsoft.lib.reflect;
 
+import java.net.URL;
+
 public interface CustomClassFinder {
 	
 	Class<?> find(String name) throws ClassNotFoundException;
+	URL resource(String name);
 	
 }
